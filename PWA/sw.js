03362c3ee1f3b426fe469639eeabf4a49063a79d
Joshua <167660585+Joshuajcx@users.noreply.github.com>
2025-11-10 16:58:33 -0600

    Add files via upload

---
+++ PWA/sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "pwa2-cache-v4";
+//Actualizar la versión de la cache y esperar un momento hasta que se vea reflejado en el sitio web
+// El programa para ser instalado me dió problemas, ver como implmentarlo correctamente
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./assets/css/style.css",
+  "./assets/js/header.js",
+  "./assets/js/main.js",
+  "./partials/header.html",
+  "./partials/footer.html",
+  "./temas/potencias/potencias-base10.html",  
+];
+
+self.addEventListener("install", (event) => {
+  console.log("Service Worker: Instalando...");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Cacheando archivos");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker: Activado");
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      )
+    )
+  );
+});
